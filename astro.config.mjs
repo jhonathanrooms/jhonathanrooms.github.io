c2c87e38304f070b7e7bbb7a5ae2de648b1d25a3
Jhonathan Salas <jhonssegura@outlook.com>
2024-12-11 20:43:22 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gianmarco.xyz/",
+  site: "https://jhonathanrooms.github.io",
   integrations: [
     sitemap(),
     robotsTxt({
